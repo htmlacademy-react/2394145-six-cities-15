8b43f53655e7ce3894059b923770362f9403e396
nikmlad <nikita.mladentsev@gmail.com>
2024-04-03 22:41:57 +0300

    Правки типизации

--- src/components/places-options/places-options.tsx
+++ src/components/places-options/places-options.tsx
@@ -6,10 +6,10 @@ type PlacesOptionsType = {
 export function PlacesOptions({setSortingType}: PlacesOptionsType): JSX.Element {
   return (
     <ul hidden className="places__options places__options--custom places__options--opened" >
-      <li className="places__option places__option--active" id='Popular' tabIndex={0} onClick={(evt) => setSortingType(evt.target.id)}>Popular</li>
-      <li className="places__option" id='Price: low to high' tabIndex={0} onClick={(evt) => setSortingType(evt.target.id)}>Price: low to high </li>
-      <li className="places__option" id='Price: high to low' tabIndex={0} onClick={(evt) => setSortingType(evt.target.id)}>Price: high to low</li>
-      <li className="places__option" id='Top rated first' tabIndex={0} onClick={(evt) => setSortingType(evt.target.id)}>Top rated first</li>
+      <li className="places__option places__option--active" id='Popular' tabIndex={0} onClick={(event) => setSortingType((event.target as Element).id)}>Popular</li>
+      <li className="places__option" id='Price: low to high' tabIndex={0} onClick={(event) => setSortingType((event.target as Element).id)}>Price: low to high </li>
+      <li className="places__option" id='Price: high to low' tabIndex={0} onClick={(event) => setSortingType((event.target as Element).id)}>Price: high to low</li>
+      <li className="places__option" id='Top rated first' tabIndex={0} onClick={(event) => setSortingType((event.target as Element).id)}>Top rated first</li>
     </ul>
   );
 }
